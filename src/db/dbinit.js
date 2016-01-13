bb175872f5959451085a459f0a85890e9dca6708
Miguel Fontes <miguel_m_fontes@hotmail.com>
2016-01-13 00:08:43 -0200

    Refatoração e remoção de bloat code

--- src/db/dbinit.js
+++ src/db/dbinit.js
@@ -1,15 +1,12 @@
 'use strict'
 module.exports = function dbInit (env, config, callback) {
-  var dbModule = config.env[env].db,
-    dbConfig = config.db[dbModule][env] || { }
+    //TODO: Tratamento de erros para o caso das configurações não existirem
+    var dbModule = config.env[env].db,
+      dbConfig = (dbModule in config.db ? config.db[dbModule][env] : { })
 
-  dbFactory(dbModule, dbConfig, callback)
-
-  function dbFactory (dbMod, dbConf, callback) {
-    // Lógica de inicialização do Database está encapsulada dentro do próprio objeto do DB
-    // passo à frente o callback. O retorno pro callback será err e o database.
-    require('./' + dbMod)
-      .build(dbConf)
-      .initialize(callback)
-  }
+  // Lógica de inicialização do Database está encapsulada dentro do próprio objeto do DB
+  // passo à frente o callback. O retorno pro callback será err e o database.
+  require('./' + dbModule)
+    .build(dbConfig)
+    .initialize(callback)
 }
